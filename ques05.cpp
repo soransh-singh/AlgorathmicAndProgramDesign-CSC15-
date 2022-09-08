0909387e70476dc43c9774cb2951f0ca14e55a45
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-09-08 08:52:38 +0530

    update
    added question 5

---
+++ ques05.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<cmath>
+
+using namespace std;
+
+double sqrt(float num){
+	float x = num;
+	for(int i=0; i<10; i++){
+		x = x - ((pow(x,2)-num)/(2*x));
+	}
+	return x;
+}
+
+int main(){
+	float num;
+	cout<<"Enter a number to find it's root: ";
+	cin>>num;
+	cout<<"Square root of a given number"<<num<<"is "<<sqrt(num)<<endl;
+	return 0;
+}
