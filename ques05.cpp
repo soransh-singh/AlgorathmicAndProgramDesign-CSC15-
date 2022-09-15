6ec84263fde65e890a8b2d94a59434f43774e4f8
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-09-15 12:41:23 +0530

    uodate

--- ques05.cpp
+++ ques05.cpp
@@ -3,10 +3,10 @@
 
 using namespace std;
 
-double sqrt(float num){
+double sqroot(float num){
 
 	float x = num;
-	for(int i=0; i<10; i++){
+	for(int i=0; i<100; i++){
 		x = x - ((pow(x,2)-num)/(2*x));
 	}
 	return x;
@@ -16,6 +16,6 @@ int main(){
 	double num;
 	cout<<"Enter a number to find it's root: ";
 	cin>>num;
-	cout<<"Square root of a given number "<<num<<" is "<<sqrt(num)<<endl;
+	cout<<"Square root of a given number "<<num<<" is "<<sqroot(num)<<endl;
 	return 0;
 }
