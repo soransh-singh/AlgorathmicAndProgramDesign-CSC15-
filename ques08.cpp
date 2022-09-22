03d9880b027f349e8fb1c21a23493c10f9779927
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-09-22 20:03:54 +0530

    update

--- ques08.cpp
+++ ques08.cpp
@@ -1,7 +1,14 @@
 #include<iostream>
 using namespace std;
 
+// convert a decimal number into
+// - Binary
+string decToBin*+Î
+
+
+// - Octal
+// - Hexadecimal
 int main(){
 
-  return 0;
+	return 0;
 }
