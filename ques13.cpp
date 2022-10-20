4a02836e1cf6975beaca521155ce59e360e0b4b4
soransh singh <57911107+soransh-singh@users.noreply.github.com>
2022-10-20 12:01:44 +0530

    update ques13

--- ques13.cpp
+++ ques13.cpp
@@ -1,8 +1,25 @@
 #include<iostream>
+#include<cmath>
 
 using namespace std;
+// Write a program to find all the prime numbers lying between 100 and 200 using user-defined function.
+
+bool isPrime(int num){
+	int term = num;
+	int termRoot = sqrt(num);
+	bool check=true; 
+	for(int i=2; i<=termRoot; i++){
+		if(term%i == 0){
+			check = false;
+		}
+	}
+	return check;
+}
 
 int main(){
-    cout<<"All the prime numbers lying between 100 and 200"<<endl;
-    return 0;
+	cout<<"All the prime numbers lying between 100 and 200 :"<<endl;
+	for(int i=100; i<=200; i++){
+		isPrime(i) && cout<<i<<endl;
+	}
+	return 0;
 }
