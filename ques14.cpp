af2fed4c9b522f48a41b2b5376dd024055550e3d
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-10-22 23:00:08 +0530

    update ques14

--- ques14.cpp
+++ ques14.cpp
@@ -3,16 +3,44 @@
 
 using namespace std;
 
-string common_subsequence(string str1, string str2){
-  // take two input
-  //compare each character of sequence to another until you find one
+// "abcde", "acbcsded"
+string common_sub(string str1, string str2){
+  string val = "";
+  int current = 0;
+  int m = str1.length();
+  int n = str2.length();
 
-  return "hello world";
+  for(int i=0 ; i<=m; i++){
+    for(int j = current; j<=n; j++){
+      if(str1[i] == str2[j]){
+        val += str2[j];
+        current = j+1;
+        break;
+      }
+    }
+  }
+    
+  return val;
+}
+
+string longest_common_sub(string str1, string str2){
+  string seq1 = common_sub(str1, str2);
+  string seq2 = common_sub(str2, str1);
+  if(seq1.length()>seq2.length())
+    return seq1;
+  else
+    return seq2;
 }
 
 
 //  Write a program to demonstrate the Longest Common Subsequence.
 int main(){
-  cout<<common_subsequence("abcde", "bacsded");
+  string str1, str2;
+  cout<<"Enter the first sequence: ";
+  cin>>str1;
+  cout<<"Enter the second sequence: ";
+  cin>>str2;
+  cout<<"Longest common subsequence between "<<str1
+    <<" and "<<str2<<" is "<<longest_common_sub( str1, str2);
   return 0;
 }
