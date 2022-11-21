f6029af9f2770e4b478d1fd436ff8d361005474c
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-11-22 00:39:06 +0530

    update

--- ques19.cpp
+++ ques19.cpp
@@ -19,7 +19,8 @@ int main(){
   }
 
   for(int i=0; i<n; i++){
-    int val = arr[i] cur=i;
+    int val = arr[i]; 
+    cur=i;
     for(int j=i-1; j>=0; j--){
       if(arr[j]>val){
         arr[j+1]=arr[j];
