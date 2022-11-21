44d6c2e16e1ce6f212cdef7e2e50d4e22744fb68
soransh <57911107+soransh-singh@users.noreply.github.com>
2022-11-21 22:57:19 +0530

    update ques 20

--- ques20.cpp
+++ ques20.cpp
@@ -2,12 +2,47 @@
 
 using namespace std;
 
+int partition(int arr[], int first, int last);
+void quicksort(int arr[], int first, int last);
+
 void quicksort(int arr[], int first, int last){
   int pivot;
-  if(low<high){
-    pivot = partition(arr, first, last)
+  if(first<last){
+
+    pivot = partition(arr, first, last);
+    quicksort(arr, first, pivot-1);
+    quicksort(arr, pivot+1, last);
   }
-  return;
+}
+
+int partition(int arr[], int first, int last){
+  int elem = arr[last];
+  int temp[last - first + 1];
+  int current=0;
+  for(int i=first; i<last; i++){
+    if(arr[i]<=elem){
+      temp[current] = arr[i];
+      current++;
+    }
+  }
+
+  temp[current]=elem;
+  int pivot = current;
+  current++;
+
+  for(int i=first; i<last; i++){
+    if(arr[i]>elem){
+      temp[current] = arr[i];
+      current++;
+    }
+  }
+
+  for(int i=0; i<=last; i++){
+    arr[first+i]=temp[i];
+  }
+
+  return pivot;
+  
 }
 
 int main(){
@@ -22,10 +57,10 @@ int main(){
     cin>>arr[i];
   }
 
-  quicksort(arr, n);
+  quicksort(arr, 0, n-1);
 
   for(int i=0; i<n; i++){
-    cout<<"the "<<i<<" element in array is: "<<arr[i];
+    cout<<"the "<<i<<" element in array is: "<<arr[i]<<endl;
   }
 
   return 0;
